d3658c05fec2948687313c4653d7eed814cedfa1
Михаил <guicokiuytr@gmail.com>
2022-06-25 03:07:33 +0600

    test_version

--- main.py
+++ main.py
@@ -40,39 +40,6 @@ async def reg(message: types.Message):
 async def receive_message(message: types.Message):
     global last_message
     last_message = message
-    # msg_for_handling = message["text"]
-    # if not moderator_access["api_id"]:
-    #     if msg_for_handling.isdigit():
-    #         moderator_access["api_id"] = int(msg_for_handling)
-    #         await bot.send_message(message.from_user.id, "api_id принят", reply_markup=markup_continue_after_id)
-    #     else:
-    #         await bot.send_message(message.from_user.id, "Формат введеных данных не верен")
-    # elif not moderator_access["api_hash"]:
-    #     if msg_for_handling.isalnum() and len(msg_for_handling) == 32:
-    #         moderator_access["api_hash"] = msg_for_handling
-    #         await bot.send_message(message.from_user.id, "api_hash принят", reply_markup=markup_continue_after_hash)
-    #     else:
-    #         await bot.send_message(message.from_user.id, "Формат введеных данных не верен")
-    # elif not moderator_access["phone_number"]:
-    #     if msg_for_handling[1:].isdigit() and len(msg_for_handling) == 12 and "+" in msg_for_handling:
-    #         moderator_access["phone_number"] = msg_for_handling
-    #         await bot.send_message(message.from_user.id, "Номер телефона принят",
-    #                                reply_markup=markup_receive_code)
-    #     else:
-    #         await bot.send_message(message.from_user.id, "Формат введеных данных не верен")
-    # elif not moderator_access["code"]:
-    #     if msg_for_handling.isdigit() and len(msg_for_handling) == 5:
-    #         moderator_access["code"] = msg_for_handling
-    #         print(moderator_access["code"])
-    #         await bot.send_message(message.from_user.id, "Код принят, пробуем подключиться")
-    #         await moderator_api.login(moderator_access["code"])
-    #         get_chat_res = await moderator_api.get_chat("https://t.me/finrequest")
-    #         if get_chat_res:
-    #             await bot.send_message(message.from_user.id, "Ваш код уже есть в системе! \nБот готов к работе!")
-    #         await moderator_api.parse(limit=20)
-    #     else:
-    #         await bot.send_message(message.from_user.id, "Формат введеных данных не верен")
-    #         print(moderator_access)
 
 
 @dp.callback_query_handler(text="continue")
